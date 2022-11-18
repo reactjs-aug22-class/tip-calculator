a92c89690736de60b8df16614c985056cdef2b88
Amr Fouad <3mrfouad@gmail.com>
2022-11-17 20:55:24 -0700

    implement basic tip calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+import { useMemo, useState } from 'react'
 
 function App() {
+  const [bill, setBill] = useState(0)
+  const [tip, setTip] = useState(0)
+  const [people, setPeople] = useState(1)
+
+  const totalTip = useMemo(() => {
+    return bill * tip * 0.01
+  }, [bill, tip])
+  const tipPerPerson = useMemo(() => totalTip / people, [totalTip, people])
+  const payPerPerson = useMemo(
+    () => (bill + totalTip) / people,
+    [bill, totalTip, people]
+  )
+
+  console.count('component re-rendered')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="bill">Bill</label>
+      <input
+        type="number"
+        id="bill"
+        min="0"
+        value={bill}
+        onChange={evt => setBill(parseInt(evt.target.value))}
+      />
+
+      <label htmlFor="tip">Tip %</label>
+      <input
+        type="number"
+        id="tip"
+        min="0"
+        value={tip}
+        onChange={evt => setTip(parseInt(evt.target.value))}
+      />
+
+      <label htmlFor="people">No. of People</label>
+      <input
+        type="number"
+        id="people"
+        min="1"
+        value={people}
+        onChange={evt => setPeople(parseInt(evt.target.value))}
+      />
+
+      <p>
+        Total tip: CAD$
+        {isNaN(totalTip) ? '-' : totalTip.toFixed(2)}
+      </p>
+      <p>
+        Tip per person: CAD$
+        {isNaN(tipPerPerson) ? '-' : tipPerPerson.toFixed(2)}
+      </p>
+      <p>
+        Total split per person: CAD$
+        {isNaN(payPerPerson) ? '-' : payPerPerson.toFixed(2)}
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
